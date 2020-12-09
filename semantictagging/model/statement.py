17842b9353e54c004e12439282b18b278b2e0b05
TerryMXJ <terrymeng0820@163.com>
2020-12-09 09:35:14 +0800

    [update]

--- semantictagging/model/statement.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-class Statement:
-    def __init__(self, tokens) -> None:
-        self.tokens = tokens
-        self.ast = None
-        self.fingerprint = -1
