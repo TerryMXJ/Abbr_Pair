2150db0d1c144352b8af7309e59b507f88491b1d
TerryMXJ <terrymeng0820@163.com>
2020-12-10 14:31:00 +0800

    [update]

--- semantictagging/miner/hypernym_base.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-class HypernymBase:
-    def __init__(self):
-        self.base = dict()
-
-     
--- semantictagging/miner/hypernym_miner.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-class HypernymMiner:
-    def __init__(self):
-        pass
