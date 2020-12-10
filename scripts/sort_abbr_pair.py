2f2f0b6b8f859cc900f9be6fadafc771a707399d
TerryMXJ <terrymeng0820@163.com>
2020-12-10 14:32:03 +0800

    [add] sort script for result data

---
+++ scripts/sort_abbr_pair.py
@@ -0,0 +1,13 @@
+import pickle
+
+file_path = 'output/abbr_pair_result_12_10.pickle'
+save_path = 'output/abbr_pair_result_sorted_12_10.txt'
+
+if __name__ == '__main__':
+    with open(file_path, 'rb') as f:
+        data: dict = pickle.load(f)
+    result: list = list((i, data[i]) for i in data.keys())
+    sorted_result: list = sorted(result, key=lambda x: x[1], reverse=True)
+    with open(save_path, 'w+') as f:
+        for i in sorted_result:
+            f.write('{} - {}\n'.format(i[0], i[1]))
